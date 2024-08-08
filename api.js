4180f010efedeeba050d7dc18e33d303e159bcdf
kodegv <celsocharlez1@gmail.com>
2024-08-08 11:57:00 +0200

    Scrimba upload

---
+++ api.js
@@ -0,0 +1,102 @@
+import { initializeApp } from "firebase/app";
+import {
+    getFirestore,
+    collection,
+    doc,
+    getDocs,
+    getDoc,
+    query,
+    where,
+    documentId
+} from "firebase/firestore/lite"
+
+// DJS08 Firebase - CCharlez
+const firebaseConfig = {
+    apiKey: "AIzaSyDeLCecSYSlR0bLH8zPXmOOzVd9_U7ltwk",
+    authDomain: "djs08-70400.firebaseapp.com",
+    projectId: "djs08-70400",
+    storageBucket: "djs08-70400.appspot.com",
+    messagingSenderId: "1087903611713",
+    appId: "1:1087903611713:web:4ddff44edd1b24ce137fc0"
+};
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+// Refactoring the fetching functions below
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef)
+    return {
+        ...snapshot.data(),
+        id: snapshot.id
+    }
+}
+
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+/* 
+This 👇 isn't normally something you'd need to do. Instead, you'd 
+set up Firebase security rules so only the currently logged-in user 
+could edit their vans.
+
+https://firebase.google.com/docs/rules
+
+I'm just leaving this here for educational purposes, as it took
+me a while to find the `documentId()` function that allows you
+to use a where() filter on a document's ID property. (Since normally
+it only looks at the data() properties of the document, meaning you
+can't do `where("id", "==", id))`
+
+It also shows how you can chain together multiple `where` filter calls
+*/
+
+// export async function getHostVan(id) {
+//     const q = query(
+//         vansCollectionRef,
+//         where(documentId(), "==", id),
+//         where("hostId", "==", "123")
+//     )
+//     const snapshot = await getDocs(q)
+//     const vans = snapshot.docs.map(doc => ({
+//         ...doc.data(),
+//         id: doc.id
+//     }))
+//     return vans[0]
+// }
+
+export async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
+}
